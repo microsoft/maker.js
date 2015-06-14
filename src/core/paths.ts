af1df90a434683483cf99fdf2c60c70399edc6f7
Dan Marshall <danmar@microsoft.com>
2015-06-14 16:45:32 -0700

    newable paths

---
+++ src/core/paths.ts
@@ -0,0 +1,56 @@
+﻿/// <reference path="path.ts" />
+
+module MakerJs.paths {
+    
+    //shortcuts
+
+    /**
+     * Class for arc path.
+     * 
+     * @param id The id of the new path.
+     * @param origin The origin of the new path, either as a point object, or as an array of numbers.
+     * @param radius The radius of the arc.
+     * @param startAngle The start angle of the arc.
+     * @param endAngle The end angle of the arc.
+     * @returns A new POJO representing an arc path.
+     */
+    export class Arc implements IPathArc {
+        public type: string;
+
+        constructor(public id: string, public origin: IPoint, public radius: number, public startAngle: number, public endAngle: number) {
+            this.type = pathType.Arc;
+        }
+    }
+
+    /**
+     * Class for circle path.
+     * 
+     * @param id The id of the new path.
+     * @param origin The origin of the new path, either as a point object, or as an array of numbers.
+     * @param radius The radius of the circle.
+     * @returns A new POJO representing an circle path.
+     */
+    export class Circle implements IPathCircle {
+        public type: string;
+
+        constructor(public id: string, public origin: IPoint, public radius: number) {
+            this.type = pathType.Circle;
+        }
+    }
+
+    /**
+     * Class for line path.
+     * 
+     * @param id The id of the new path.
+     * @param origin The origin of the new path, either as a point object, or as an array of numbers.
+     * @param end The end point of the line.
+     * @returns A new POJO representing an line path.
+     */
+    export class Line implements IPathLine {
+        public type: string;
+
+        constructor(public id: string, public origin: IPoint, public end: IPoint) {
+            this.type = pathType.Line;
+        }
+    }
+}
