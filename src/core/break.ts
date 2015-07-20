584aac2201dc603dd390b4097605bb8504bfb73e
Dan Marshall <danmar@microsoft.com>
2015-07-19 17:01:47 -0700

    added path.breakAtPoint

---
+++ src/core/break.ts
@@ -0,0 +1,65 @@
+﻿/// <reference path="path.ts" />
+
+module MakerJs.path {
+
+    /**
+     * @private
+     */
+    interface IBreakPathFunctionMap {
+        [type: string]: (path: IPath, pointOfBreak: IPoint) => IPath;
+    }
+
+    var breakPathFunctionMap: IBreakPathFunctionMap = {};
+
+    breakPathFunctionMap[pathType.Arc] = function (arc: IPathArc, pointOfBreak: IPoint): IPath {
+
+        var angleAtBreakPoint = angle.toDegrees(angle.ofPointInRadians(arc.origin, pointOfBreak));
+
+        var savedEndAngle = arc.endAngle;
+
+        arc.endAngle = angleAtBreakPoint;
+
+        return new paths.Arc(arc.origin, arc.radius, angleAtBreakPoint, savedEndAngle);
+    };
+
+    breakPathFunctionMap[pathType.Circle] = function (circle: IPathCircle, pointOfBreak: IPoint): IPath {
+        circle.type = pathType.Arc;
+
+        var arc: IPathArc = <IPathArc>circle;
+
+        var angleAtBreakPoint = angle.toDegrees(angle.ofPointInRadians(circle.origin, pointOfBreak));
+
+        arc.startAngle = angleAtBreakPoint;
+        arc.endAngle = angleAtBreakPoint + 360;
+
+        return null;
+    };
+
+    breakPathFunctionMap[pathType.Line] = function (line: IPathLine, pointOfBreak: IPoint): IPath {
+
+        var savedEndPoint = line.end;
+
+        line.end = pointOfBreak;
+
+        return new paths.Line(pointOfBreak, savedEndPoint);
+    };
+
+    /**
+     * Breaks a path in 2. The original path will end at the supplied pointOfBreak, 
+     * a new path is returned which begins at the pointOfBreak and ends at the original end point.
+     * For Circle, the original path will be converted in place to an Arc, and null is returned.
+     * 
+     * @param pathToBreak The path to break.
+     * @param pointOfBreak The point at which to break the path.
+     */
+    export function breakAtPoint(pathToBreak: IPath, pointOfBreak: IPoint): IPath {
+
+        var fn = breakPathFunctionMap[pathToBreak.type];
+        if (fn) {
+            return fn(pathToBreak, pointOfBreak);
+        }
+
+        return null;
+    }
+
+}
