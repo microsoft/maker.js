8be48a71a7e84711d11dcc2ef6cc5a9401afd7e1
Dan Marshall <danmar@microsoft.com>
2015-08-01 18:10:19 -0700

    Added isPathLine, isPathCircle, isPathArc

--- src/core/maker.ts
+++ src/core/maker.ts
@@ -174,6 +174,15 @@ module MakerJs {
     }
 
     /**
+     * Test to see if an object implements the required properties of a line.
+     * 
+     * @param item The item to test.
+     */
+    export function isPathLine(item: any): boolean {
+        return isPath(item) && item.type == pathType.Line && item.end;
+    }
+
+    /**
      * A circle path.
      * 
      * Examples:
@@ -188,6 +197,15 @@ module MakerJs {
          * The radius of the circle.
          */
         radius: number;
+    }
+
+    /**
+     * Test to see if an object implements the required properties of a circle.
+     * 
+     * @param item The item to test.
+     */
+    export function isPathCircle(item: any): boolean {
+        return isPath(item) && item.type == pathType.Circle && item.radius;
     }
 
     /**
@@ -210,6 +228,15 @@ module MakerJs {
          * The angle (in degrees) to end drawing the arc, in polar (counter-clockwise) direction. May be less than start angle if it past 360.
          */
         endAngle: number;
+    }
+
+    /**
+     * Test to see if an object implements the required properties of an arc.
+     * 
+     * @param item The item to test.
+     */
+    export function isPathArc(item: any): boolean {
+        return isPath(item) && item.type == pathType.Arc && item.radius && item.startAngle && item.endAngle;
     }
 
     /**
@@ -362,6 +389,8 @@ module MakerJs {
     }
 
 }
+
+declare var module; 
 
 //CommonJs
 module.exports = MakerJs;
