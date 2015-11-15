805d5926fc631963de397c50219a972474d85fb4
Dan Marshall <danmar@microsoft.com>
2015-11-15 13:41:17 -0800

    reversed order of rounding operation, added accuracy to ange.areEqual()

--- src/core/angle.ts
+++ src/core/angle.ts
@@ -9,9 +9,9 @@ module MakerJs.angle {
      * @param b Second angle.
      * @returns true if angles are the same, false if they are not
      */
-    export function areEqual(angle1: number, angle2: number) {
-        var a1 = noRevolutions(round(angle1));
-        var a2 = noRevolutions(round(angle2));
+    export function areEqual(angle1: number, angle2: number, accuracy?: number) {
+        var a1 = round(noRevolutions(angle1), accuracy);
+        var a2 = round(noRevolutions(angle2), accuracy);
 
         return a1 == a2 || a1 + 360 == a2 || a1 - 360 == a2;
     }
