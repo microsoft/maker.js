529a3f44e1b8b6bfbd514fbefce315a358e167de
Dan Marshall <danmar@microsoft.com>
2015-12-16 22:36:13 -0800

    Added self-intersect option to OvalArc

---
+++ examples/arc_test.js
@@ -0,0 +1,11 @@
+﻿var arc_test = makerjs.models.OvalArc;
+
+arc_test.metaParameters = [
+    { title: "start angle", type: "range", min: -360, max: 360, step: 1, value: -180 },
+    { title: "end angle", type: "range", min: -360, max: 360, step: 1, value: 170 },
+    { title: "sweep", type: "range", min: 0, max: 100, step: 1, value: 50 },
+    { title: "radius", type: "range", min: 0, max: 100, step: 1, value: 10 },
+    { title: "self intersect", type: "bool", value: false }
+];
+
+module.exports = arc_test;
