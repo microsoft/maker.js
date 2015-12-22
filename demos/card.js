0ec62315ec6b70a2d6925df4e7c935239cd393c9
Dan Marshall <danmar@microsoft.com>
2015-12-21 19:28:36 -0800

    temp fix to card

--- demos/card.js
+++ demos/card.js
@@ -10,132 +10,152 @@ var model = makerjs.model;
 
 function logo(or, ir, ear, outline, mHeight, serifHeight, speed, drop, columnWidth, spacing, step) {
 
-    if (arguments.length == 0) {
-        var v = makerjs.kit.getParameterValues(logo);
-        or = v.shift();
-        ir = v.shift();
-        ear = v.shift();
-        outline = v.shift();
-        mHeight = v.shift();
-        serifHeight = v.shift();
-        speed = v.shift();
-        drop = v.shift();
-        columnWidth = v.shift();
-        spacing = v.shift();
-        step = v.shift();
-    }
+    var point = makerjs.point;
+    var path = makerjs.path;
+    var paths = makerjs.paths;
 
-    function M() {
-        this.models = {
-            base: {
-                paths: {}
-            },
-            legs: {
-                models: {}
-            }
-        };
-    }
+    function bend(r, bendTop, x, trimTo, outer) {
 
-    var m_letter = new M();
-    var m_outline = new M();
-    
-    var legModels = m_letter.models.legs.models;
-    var outlineModels = m_outline.models.legs.models;
+        outer = outer || 0;
 
-    var far = 100;
+        var hguide = new paths.Line([0, bendTop - r], [100, bendTop - r]);
+        var vguide = path.rotate(new paths.Line([x, 0], [x, 100]), -speed, [x, 0]);
+        var intersectionPoint = path.intersection(hguide, vguide).intersectionPoints[0];
+        var center = point.subtract(intersectionPoint, [makerjs.solvers.solveTriangleASA(90, r, speed), 0]);
 
-    function addLeg(id, leftRef, leftSpace, topRef, topSpace, topPoint, trimToLeftRef, earDistance) {
-        var leg = {
-            paths: {}
-        };
+        var arc = new paths.Arc(center, r + outer, - speed, 90 + drop);
 
-        leg.paths.top = new Parallel(topRef, topSpace, topPoint);
-        leg.paths.left = new Parallel(leftRef, leftSpace, [far, 0]);
-        leg.paths.serif = new Parallel(leg.paths.top, serifHeight, [0, 0]);
-        leg.paths.right = new Parallel(leg.paths.left, columnWidth, [far, 0]);
-        leg.paths.ear = new Parallel(leg.paths.left, earDistance, [0, 0]);
-
-        legModels[id] = leg;
-
-        var outleg = {
-            paths: {}
-        };
-
-        outleg.paths.top = new Parallel(leg.paths.top, outline, [0, far]);
-        outleg.paths.left = new Parallel(leg.paths.left, outline, [-far, 0]);
-        outleg.paths.serif = new Parallel(leg.paths.serif, outline, [0, 0]);
-        outleg.paths.right = new Parallel(leg.paths.right, outline, [far, 0]);
-        outleg.paths.ear = new Parallel(leg.paths.ear, outline, [-far, 0]);
-
-        outlineModels[id] = outleg;
-    }
-
-    function trimLeg(id) {
-
-        function trimLegPart(leg, innerRadius, outerRadius) {
-            trimLines(leg.paths.top, leg.paths.right);
-            trimLines(leg.paths.serif, leg.paths.left);
-            leg.paths.innerFillet = path.fillet(leg.paths.left, leg.paths.serif, innerRadius);
-            leg.paths.outerFillet = path.fillet(leg.paths.top, leg.paths.right, outerRadius);
-            trimLines(leg.paths.top, leg.paths.ear, true);
-            trimLines(leg.paths.serif, leg.paths.ear, true, true);
+        var Horizontal = path.rotate(
+                new paths.Line([-10, arc.origin[1] + r + outer], point.add(arc.origin, [0, r + outer])),
+                drop,
+                arc.origin
+            );
+        
+        if (!outer) {
+            trimLine(Horizontal, 'origin', trimTo);
         }
 
-        trimLegPart(legModels[id], ir, or);
-        trimLegPart(outlineModels[id], ir - outline, or + outline);
+        var arcPoints = point.fromArc(arc);
+
+        var Vertical = new paths.Line([x + makerjs.solvers.solveTriangleASA(90, outer, speed), 0], arcPoints[0]);
+
+        if (!outer) {
+            trimLine(Vertical, 'origin', bottomGuide);
+        }
+
+        this.paths = {
+            arc: arc,
+            Horizontal: Horizontal,
+            Vertical: Vertical
+        };
     }
 
-    function combineM(m) {
-        var legs = m.models.legs;
-        model.combine(legs.models['1'], legs.models['2'], false, true, false, true);
-        model.combine(legs.models['2'], legs.models['3'], false, true, false, true);
-        model.combine(m.models.base, legs, true, false, false, true);
+    function leg(legTop, xOffset, trimTo) {
+
+        this.models = {
+            b1: new bend(ir, outline + legTop - serifHeight, speedOutline + xOffset, trimTo),
+            b2: new bend(or, outline + legTop, speedOutline + columnWidth + xOffset, trimTo),
+            b3: new bend(or, outline + legTop, speedOutline + columnWidth + xOffset, trimTo, outline)
+        };
+
+        this.paths = {
+            legBottom: new paths.Line(this.models.b1.paths.Vertical.origin, this.models.b2.paths.Vertical.origin)
+        };
     }
 
-    m_outline.models.base.paths.bottom = new Line([0, 0], [far, 0]);
-    m_letter.models.base.paths.bottom = new Parallel(m_outline.models.base.paths.bottom, outline, [0, far]);
+    var speedOutline = makerjs.solvers.solveTriangleASA(90, outline, speed);
 
-    var rotatedLeft = path.rotate(new Line([0, 0], [0, far]), -speed, [0, 0]);
-    var rotatedBottom = path.rotate(new Line([-far, outline], [far, outline]), drop, [0, outline]);
+    var bottomGuide = new paths.Line([0, outline], [100, outline]);
 
-    addLeg('1', rotatedLeft, outline, rotatedBottom, mHeight, [0, far], false, ear + ir);
-    addLeg('2', legModels['1'].paths.right, spacing, legModels['1'].paths.top, step, [0, 0], true, spacing + columnWidth / 2);
-    addLeg('3', legModels['2'].paths.right, spacing, legModels['2'].paths.top, step, [0, 0], true, spacing + columnWidth / 2);
+    var earline = path.rotate(new paths.Line([-ear, 0], [-ear, 100]), -speed, [-ear, 0]);
 
-    trimLeg('1', false);
-    trimLeg('2', true);
-    trimLeg('3', true);
+    var leg1 = new leg(mHeight, 0, earline);
+    var leg2 = new leg(mHeight - step, columnWidth + spacing, leg1.models.b2.paths.Vertical);
+    var leg3 = new leg(mHeight - 2 * step, 2 * (columnWidth + spacing), leg2.models.b2.paths.Vertical);
 
-    combineM(m_letter);
-    combineM(m_outline);
+    var outBottom = new paths.Line([0, 0], leg3.models.b3.paths.Vertical.origin);
 
-    this.models = {
-        letter: m_letter,
-        outline: m_outline
+    var earPivot = leg1.models.b1.paths.Horizontal.origin;
+    var earH = path.rotate(new paths.Line(point.subtract(earPivot, [100, outline]), point.subtract(earPivot, [-100, outline])), drop, earPivot);
+    var outHome = trimLine(path.rotate(new paths.Line([0, 0], [0, 100]), -speed, [0, 0]), 'end', earH);
+    var earOutline = trimLine(path.rotate(new paths.Line([-ear - speedOutline, 0], [-ear - speedOutline, 100]), -speed, [-ear - speedOutline, 0]), 'origin', earH);
+
+    trimLines(earOutline, 'end', leg1.models.b3.paths.Horizontal, 'origin');
+
+    trimBends(leg1.models.b3, leg2.models.b3);
+    trimBends(leg2.models.b3, leg3.models.b3);
+
+    this.paths = {
+        ear: new paths.Line(leg1.models.b1.paths.Horizontal.origin, leg1.models.b2.paths.Horizontal.origin),
+        leg1bottom: new paths.Line(leg1.models.b2.paths.Vertical.origin, leg2.models.b1.paths.Horizontal.origin),
+        leg2bottom: new paths.Line(leg2.models.b2.paths.Vertical.origin, leg3.models.b1.paths.Horizontal.origin),
+        outHome: outHome,
+        earOutline: earOutline,
+        earOutH: new paths.Line(earOutline.origin, outHome.end),
+        outBottom: outBottom
     };
 
+    this.models = {
+        leg1: leg1,
+        leg2: leg2,
+        leg3: leg3
+    };
+
+    leg1.models.b2.paths.Vertical.origin = leg2.models.b2.paths.Horizontal.origin;
+    leg2.models.b2.paths.Vertical.origin = leg3.models.b2.paths.Horizontal.origin;
 }
 
-function trimLines(line1, line2, useLine1Origin, useLine2Origin) {
-    var int = path.slopeIntersectionPoint(line1, line2);
-    if (int) {
-        line1[useLine1Origin ? 'origin' : 'end'] = int;
-        line2[useLine2Origin ? 'origin' : 'end'] = int;
+function trimLine(line, propertyName, trimToPath) {
+    var intersection = makerjs.path.intersection(line, trimToPath);
+    if (intersection) {
+        line[propertyName] = intersection.intersectionPoints[0];
+    }
+    return line;
+}
+
+function trimLines(line1, propertyName1, line2, propertyName2) {
+    var intersection = makerjs.path.intersection(line1, line2);
+    if (intersection) {
+        line1[propertyName1] = intersection.intersectionPoints[0];
+        line2[propertyName2] = intersection.intersectionPoints[0];
+    }
+    return intersection;
+}
+
+function trimBends(b1, b2) {
+    var intersection = trimLines(b1.paths.Vertical, 'origin', b2.paths.Horizontal, 'origin');
+    if (intersection) return;
+
+    intersection = makerjs.path.intersection(b1.paths.arc, b2.paths.Horizontal);
+    if (intersection) {
+        b1.paths.arc.startAngle = intersection.path1Angles[0];
+        b2.paths.Horizontal.origin = intersection.intersectionPoints[0];
+        delete b1.paths.Vertical;
+        return;
+    }
+
+    intersection = makerjs.path.intersection(b1.paths.arc, b2.paths.arc);
+    if (intersection) {
+        b1.paths.arc.startAngle = intersection.path1Angles[0];
+        b2.paths.arc.endAngle = intersection.path2Angles[0];
+        delete b1.paths.Vertical;
+        delete b2.paths.Horizontal;
+        return;
     }
 }
 
 logo.metaParameters = [
     { title: "outer radius", type: "range", min: 0, max: 1.7, step: .1, value: 1.06 },
     { title: "inner radius", type: "range", min: 0, max: .9, step: .1, value: .3 },
-    { title: "ear", type: "range", min: 0, max: 2, step: .1, value: 1.1 },
+    { title: "ear", type: "range", min: .3, max: 2, step: .1, value: .35 },
     { title: "outline", type: "range", min: 0.2, max: 2, step: .1, value: 1.06 },
-    { title: "m height", type: "range", min: 7, max: 20, step: .1, value: 8.3 },
+    { title: "m height", type: "range", min: 7, max: 10, step: .1, value: 8.3 },
     { title: "serif height", type: "range", min: .1, max: 1.9, step: .1, value: .65 },
     { title: "speed", type: "range", min: 0, max: 45, step: 1, value: 19.01 },
-    { title: "drop", type: "range", min: 0, max: 30, step: 1, value: 1 },
-    { title: "column width", type: "range", min: .4, max: 5, step: .1, value: 2.59 },
-    { title: "spacing", type: "range", min: 1.3, max: 5, step: .1, value: 1.25 },
-    { title: "step", type: "range", min: 0, max: 4, step: .1, value: 2.385 },
+    { title: "drop", type: "range", min: 0, max: 12, step: 1, value: 1 },
+    { title: "column width", type: "range", min: .4, max: 5, step: .1, value: 2.7 },
+    { title: "spacing", type: "range", min: 1.3, max: 5, step: .1, value: 1.32 },
+    { title: "step", type: "range", min: 1.5, max: 2.7, step: .1, value: 2.31 },
 ];
 
 module.exports = logo;
