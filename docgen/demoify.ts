40defc92067b636ae14bb61f2b2f826ef1e0eba7
Dan Marshall <danmar@microsoft.com>
2015-12-23 13:06:52 -0800

    typescripted demoify.js added list.html for thumbnails

--- docgen/demoify.ts
+++ docgen/demoify.ts
@@ -1,11 +1,14 @@
-/// <reference path="../typings/node/node.d.ts" />
-var fs = require('fs');
+﻿/// <reference path="../typings/node/node.d.ts" />
+
+import fs = require('fs');
 var browserify = require('browserify');
 var pjson = require('../package.json');
 var makerjs = require('../target/js/node.maker.js');
+
 var prefix = 'makerjs-';
 var prefixLen = prefix.length;
-function demoify(name) {
+
+function demoify(name: string) {
     var filename = './demos/' + name + '.js';
     console.log('writing ' + filename);
     var b = browserify();
@@ -13,32 +16,43 @@ function demoify(name) {
     b.require(prefix + name, { expose: name });
     b.bundle().pipe(fs.createWriteStream(filename));
 }
-function getRequireConstructor(name) {
+
+function getRequireConstructor(name: string) {
     return require(prefix + name);
 }
-function thumbnail(name, constructor) {
+
+function thumbnail(name: string, constructor) {
     var parameters = makerjs.kit.getParameterValues(constructor);
     var model = makerjs.kit.construct(constructor, parameters);
+
     var svg = makerjs.exporter.toSVG(model);
+
     var attrs = { 'class': 'thumb', 'title': name };
+
     var x = new makerjs.exporter.XmlTag('div', attrs);
     x.innerText = svg;
     x.innerTextEscaped = true;
+
     return x.toString();
 }
+
 function main() {
+
     var listFile = fs.openSync('./demos/list.html', 'w');
     fs.write(listFile, '---\nlayout: page\ntitle: Demos\n---\n');
+
     for (var key in pjson.dependencies) {
         if (key.indexOf(prefix) == 0) {
-            var name = key.substring(prefixLen);
+            var name = <string>key.substring(prefixLen);
             demoify(name);
+
             var ctor = getRequireConstructor(name);
             fs.write(listFile, thumbnail(name, ctor));
             fs.write(listFile, '\n\n');
         }
     }
+
     fs.closeSync(listFile);
 }
+
 main();
-//# sourceMappingURL=demoify.js.map
