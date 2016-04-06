fef30254ea455c4f15b2e7e42b24dc8e76fa2e3b
Dan Marshall <danmar@microsoft.com>
2016-04-06 08:29:23 -0700

    update typings

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,4 @@
-
+/// <reference path="codemirror/codemirror.d.ts" />
+/// <reference path="marked/marked.d.ts" />
 /// <reference path="node/node.d.ts" />
-/// <reference path="OpenJsCad/csg.d.ts" />
+/// <reference path="openjscad/csg.d.ts" />
