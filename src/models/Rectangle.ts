dabfdbee981ea2a35473b0cfbf43166cc8a277b2
Dan Marshall <danmar@microsoft.com>
2016-04-24 23:18:13 -0700

    add constructor for IMeasure

--- src/models/Rectangle.ts
+++ src/models/Rectangle.ts
@@ -2,8 +2,26 @@ namespace MakerJs.models {
     export class Rectangle implements IModel {
 
         public paths: IPathMap = {};
+        public origin: IPoint;
         
-        constructor(width: number, height: number) {
+        constructor(width: number, height: number);
+        constructor(measurement: IMeasure);
+        constructor(...args: any[]) {
+            var width: number;
+            var height: number;
+
+            if (args.length == 2) {
+                width = args[0];
+                height = args[1];
+            } else {
+                //use measurement
+                var m = args[0] as IMeasure;
+                this.origin = m.low;
+
+                width = m.high[0] - m.low[0];
+                height = m.high[1] - m.low[1];
+            }
+
             this.paths = new ConnectTheDots(true, [[0, 0], [width, 0], [width, height], [0, height]]).paths;
         }
     }
