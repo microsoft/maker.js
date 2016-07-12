667bb50b588aa6952d686215a21dc990a1ea3bfe
Dan Marshall <danmar@microsoft.com>
2016-07-12 00:05:44 -0700

    tsd update

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -3,3 +3,5 @@
 /// <reference path="OpenJsCad/csg.d.ts" />
 /// <reference path="pdfkit/pdfkit.d.ts" />
 /// <reference path="text-encoding/text-encoding.d.ts" />
+/// <reference path="codemirror/codemirror.d.ts" />
+/// <reference path="marked/marked.d.ts" />
