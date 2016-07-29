35eb7e01a98d902a8af466b6ee203c83bd5ab6c8
Dan Marshall <danmar@microsoft.com>
2016-07-29 00:13:57 -0700

    Added EllipticArc

--- src/models/Ellipse.ts
+++ src/models/Ellipse.ts
@@ -2,6 +2,12 @@ namespace MakerJs.models {
 
     /**
      * @private
+     * Our maximum circular arc span for accurate representation by a cubic curve.
+     */
+    var maxBezierArcspan = 45;
+
+    /**
+     * @private
      */
     function controlYForCircularCubic(arcSpanInRadians: number): number {
 
@@ -33,12 +39,17 @@ namespace MakerJs.models {
     /**
      * @private
      */
-    function bezierPointsFromArc(arc: IPathArc): IPoint[] {
+    function bezierSeedFromArc(arc: IPathArc): IPathBezierSeed {
         var span = angle.ofArcSpan(arc);
         if (span <= 90) {
             var endPoints = point.fromPathEnds(arc);
             var controls = controlPointsForCircularCubic(arc);
-            return [endPoints[0], controls[0], controls[1], endPoints[1]];
+            return {
+                type: pathType.BezierSeed,
+                origin: endPoints[0],
+                controls: controls,
+                end: endPoints[1]
+            };
         }
         return null;
     }
@@ -46,16 +57,22 @@ namespace MakerJs.models {
     /**
      * @private
      */
-    function scaleDim(bezierPoints: IPoint[], i: number, s: number) {
-        bezierPoints.map(function (p) { p[i] *= s; });
+    function scaleDim(seed: IPathBezierSeed, i: number, s: number) {
+        function d(p: IPoint) {
+            p[i] *= s;
+        }
+
+        d(seed.origin);
+        seed.controls.forEach(d);
+        d(seed.end);
     }
 
     /**
      * @private
      */
-    function scaleXY(bezierPoints: IPoint[], x: number, y: number = x) {
-        scaleDim(bezierPoints, 0, x);
-        scaleDim(bezierPoints, 1, y);
+    function scaleXY(seed: IPathBezierSeed, x: number, y: number = x) {
+        scaleDim(seed, 0, x);
+        scaleDim(seed, 1, y);
     }
 
     export class Ellipse implements IModel {
@@ -63,12 +80,39 @@ namespace MakerJs.models {
         public models: IModelMap = {};
         public origin: IPoint;
 
+        /**
+         * Class for Ellipse created with 2 radii.
+         * 
+         * @param radiusX The x radius of the ellipse.
+         * @param radiusY The y radius of the ellipse.
+         * @param accuracy Optional accuracy of the underlying BezierCurve.
+         */
         constructor(radiusX: number, radiusY: number, accuracy?: number);
+
+        /**
+         * Class for Ellipse created at a specific origin and 2 radii.
+         * 
+         * @param origin The center of the ellipse.
+         * @param radiusX The x radius of the ellipse.
+         * @param radiusY The y radius of the ellipse.
+         * @param accuracy Optional accuracy of the underlying BezierCurve.
+         */
         constructor(origin: IPoint, radiusX: number, radiusY: number, accuracy?: number);
+
+        /**
+         * Class for Ellipse created at a specific x, y and 2 radii.
+         *
+         * @param cx The x coordinate of the center of the ellipse.
+         * @param cy The y coordinate of the center of the ellipse.
+         * @param rX The x radius of the ellipse.
+         * @param rY The y radius of the ellipse.
+         * @param accuracy Optional accuracy of the underlying BezierCurve.
+         */
         constructor(cx: number, cy: number, rx: number, ry: number, accuracy?: number);
+
         constructor(...args: any[]) {
 
-            var n = 8;
+            var n = 360 / maxBezierArcspan;
             var accuracy: number;
 
             var isPointArgs0 = isPoint(args[0]);
@@ -100,37 +144,37 @@ namespace MakerJs.models {
                 //clone and rotate to complete a circle
                 for (var i = 0; i < n; i++) {
 
-                    var bezierPoints = bezierPointsFromArc(arc);
+                    var seed = bezierSeedFromArc(arc);
 
                     switch (numArgs) {
                         case 1:
                             //radius
-                            scaleXY(bezierPoints, args[0] as number);
+                            scaleXY(seed, args[0] as number);
                             break;
 
                         case 2:
                             if (isPointArgs0) {
                                 //origin, radius
-                                scaleXY(bezierPoints, args[1] as number);
+                                scaleXY(seed, args[1] as number);
 
                             } else {
                                 //rx, ry
-                                scaleXY(bezierPoints, args[0] as number, args[1] as number);
+                                scaleXY(seed, args[0] as number, args[1] as number);
                             }
                             break;
 
                         case 3:
                             //origin, rx, ry
-                            scaleXY(bezierPoints, args[1] as number, args[2] as number);
+                            scaleXY(seed, args[1] as number, args[2] as number);
                             break;
 
                         case 4:
                             //cx, cy, rx, ry
-                            scaleXY(bezierPoints, args[2] as number, args[3] as number);
+                            scaleXY(seed, args[2] as number, args[3] as number);
                             break;
                     }
 
-                    this.models['Curve_' + (1 + i)] = new BezierCurve(bezierPoints, accuracy);
+                    this.models['Curve_' + (1 + i)] = new BezierCurve(seed, accuracy);
 
                     arc.startAngle += a;
                     arc.endAngle += a;
@@ -170,6 +214,74 @@ namespace MakerJs.models {
     }
 
     (<IKit>Ellipse).metaParameters = [
+        { title: "radiusX", type: "range", min: 1, max: 50, value: 50 },
+        { title: "radiusY", type: "range", min: 1, max: 50, value: 25 }
+    ];
+
+    export class EllipticArc implements IModel {
+        public models: IModelMap = {};
+
+        /**
+         * Class for Elliptic Arc created by distorting a circular arc.
+         *
+         * @param arc The circular arc to use as the basis of the elliptic arc.
+         * @param radiusX The x radius of the ellipse.
+         * @param radiusY The y radius of the ellipse.
+         * @param accuracy Optional accuracy of the underlying BezierCurve.
+         */
+        constructor(startAngle: number, endAngle: number, radiusX: number, radiusY: number, accuracy?: number);
+
+        /**
+         * Class for Elliptic Arc created by distorting a circular arc.
+         *
+         * @param arc The circular arc to use as the basis of the elliptic arc.
+         * @param distortX The x scale of the ellipse.
+         * @param distortY The y scale of the ellipse.
+         * @param accuracy Optional accuracy of the underlying BezierCurve.
+         */
+        constructor(arc: IPathArc, distortX: number, distortY: number, accuracy?: number);
+
+        constructor(...args: any[]) {
+
+            var arc: IPathArc;
+            var accuracy: number;
+            var distortX: number;
+            var distortY: number;
+
+            if (isPathArc(args[0])) {
+                arc = args[0] as IPathArc;
+                distortX = args[1] as number;
+                distortY = args[2] as number;
+                accuracy = args[3] as number;
+            } else {
+                arc = new paths.Arc([0, 0], 1, args[0] as number, args[1] as number);
+                distortX = args[2] as number;
+                distortY = args[3] as number;
+                accuracy = args[4] as number;
+            }
+
+            var span = angle.ofArcSpan(arc);
+
+            //split into equal chunks, no larger than max chunk size
+            var count = Math.ceil(span / maxBezierArcspan);
+            var subSpan = span / count;
+            var subArc = path.clone(arc) as IPathArc;
+
+            for (var i = 0; i < count; i++) {
+                subArc.startAngle = arc.startAngle + (i * subSpan);
+                subArc.endAngle = subArc.startAngle + subSpan;
+
+                var seed = bezierSeedFromArc(subArc);
+                scaleXY(seed, distortX, distortY);
+
+                this.models['Curve_' + (1 + i)] = new BezierCurve(seed, accuracy);
+            }
+        }
+    }
+
+    (<IKit>EllipticArc).metaParameters = [
+        { title: "startAngle", type: "range", min: 0, max: 90, value: 0 },
+        { title: "endAngle", type: "range", min: 90, max: 360, value: 180 },
         { title: "radiusX", type: "range", min: 1, max: 50, value: 50 },
         { title: "radiusY", type: "range", min: 1, max: 50, value: 25 }
     ];
