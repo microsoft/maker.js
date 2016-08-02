540947cdaacdae3228e2e0172c1d930c9857a240
Dan Marshall <danmar@microsoft.com>
2016-08-01 20:16:52 -0700

    added importer

--- src/models/ConnectTheDots.ts
+++ src/models/ConnectTheDots.ts
@@ -4,7 +4,40 @@ namespace MakerJs.models {
 
         public paths: IPathMap = {};
 
-        constructor(isClosed: boolean, points: IPoint[]) {
+        constructor(numericList: string);
+
+        constructor(coords: number[]);
+
+        constructor(isClosed: boolean, points: IPoint[]);
+
+        constructor(...args: any[]) {
+
+            var isClosed: boolean;
+            var points: IPoint[];
+
+            switch (args.length) {
+                case 1:
+                    isClosed = true;
+
+                    var coords: number[];
+
+                    if (Array.isArray(args[0])) {
+                        coords = args[0] as number[];
+                    } else {
+                        coords = importer.parseNumericList(args[0] as string);
+                    }
+                    
+                    points = [];
+                    for (var i = 0; i < coords.length; i += 2) {
+                        points.push([coords[i], coords[i + 1]]);
+                    }
+                    break;
+
+                case 2:
+                    isClosed = args[0] as boolean;
+                    points = args[1] as IPoint[];
+                    break;
+            }
 
             var connect = (a: number, b: number) => {
                 this.paths["ShapeLine" + i] = new paths.Line(points[a], points[b]);
@@ -24,7 +57,7 @@ namespace MakerJs.models {
         { title: "closed", type: "bool", value: true },
         {
             title: "points", type: "select", value: [
-                [[0, 0], [40, 40], [60, 20], [100, 100], [60, 60], [40, 80] ],
+                [[0, 0], [40, 40], [60, 20], [100, 100], [60, 60], [40, 80]],
                 [[0, 0], [100, 0], [50, 87]]
             ]
         }
