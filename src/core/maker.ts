544c934c40d20f987808b017247e788d54c12714
Dan Marshall <danmar@microsoft.com>
2016-08-02 23:12:18 -0700

    add comments for new type functions

--- src/core/maker.ts
+++ src/core/maker.ts
@@ -100,14 +100,32 @@ namespace MakerJs {
         return target;
     }
 
+    /**
+     * Test to see if a variable is a function.
+     * 
+     * @param value The object to test.
+     * @returns True if the object is a function type.
+     */
     export function isFunction(value: any): boolean {
         return typeof value === 'function';
     }
 
+    /**
+     * Test to see if a variable is a number.
+     * 
+     * @param value The object to test.
+     * @returns True if the object is a number type.
+     */
     export function isNumber(value: any): boolean {
         return typeof value === 'number';
     }
 
+    /**
+     * Test to see if a variable is an object.
+     * 
+     * @param value The object to test.
+     * @returns True if the object is an object type.
+     */
     export function isObject(value: any): boolean {
         return typeof value === 'object';
     }
