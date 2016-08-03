7f4a53416e6ce97c7b3bb97cc608657ac547ecaa
Dan Marshall <danmar@microsoft.com>
2016-08-02 21:41:00 -0700

    added constructor to surround a model.

--- src/models/Rectangle.ts
+++ src/models/Rectangle.ts
@@ -3,23 +3,74 @@ namespace MakerJs.models {
 
         public paths: IPathMap = {};
         public origin: IPoint;
-        
+
+        /**
+         * Create a rectangle from width and height.
+         * 
+         * Example:
+         * ```
+         * var r = new makerjs.models.Rectangle(100, 50);
+         * ```
+         * 
+         * @param width Width of the rectangle.
+         * @param height Height of the rectangle.
+         */
         constructor(width: number, height: number);
+
+        /**
+         * Create a rectangle which will surround a model.
+         * 
+         * Example:
+         * ```
+         * var e = new makerjs.models.Ellipse(17, 10); // draw an ellipse so we have something to surround.
+         * var r = new makerjs.models.Rectangle(e, 3); // draws a rectangle surrounding the ellipse by 3 units.
+         * ```
+         * 
+         * @param modelToSurround IModel object.
+         * @param margin Optional distance from the model.
+         */
+        constructor(modelToSurround: IModel, margin?: number);
+
+        /**
+         * Create a rectangle from a measurement.
+         * 
+         * Example:
+         * ```
+         * var e = new makerjs.models.Ellipse(17, 10); // draw an ellipse so we have something to measure.
+         * var m = makerjs.measure.modelExtents(e);    // measure the ellipse.
+         * var r = new makerjs.models.Rectangle(m);    // draws a rectangle surrounding the ellipse.
+         * ```
+         * 
+         * @param measurement IMeasure object. See http://microsoft.github.io/maker.js/docs/api/modules/makerjs.measure.html#pathextents and http://microsoft.github.io/maker.js/docs/api/modules/makerjs.measure.html#modelextents to get measurements of paths and models.
+         */
         constructor(measurement: IMeasure);
+
         constructor(...args: any[]) {
             var width: number;
             var height: number;
 
-            if (args.length == 2) {
-                width = args[0];
-                height = args[1];
+            if (args.length === 2 && !isObject(args[0])) {
+                width = args[0] as number;
+                height = args[1] as number;
             } else {
-                //use measurement
-                var m = args[0] as IMeasure;
-                this.origin = m.low;
 
-                width = m.high[0] - m.low[0];
-                height = m.high[1] - m.low[1];
+                var margin = 0;
+                var m: IMeasure;
+
+                if (isModel(args[0])) {
+                    m = measure.modelExtents(args[0] as IModel);
+                    if (args.length === 2) {
+                        margin = args[1] as number;
+                    }
+                } else {
+                    //use measurement
+                    m = args[0] as IMeasure;
+                }
+
+                this.origin = point.subtract(m.low, [margin, margin]);
+
+                width = m.high[0] - m.low[0] + 2 * margin;
+                height = m.high[1] - m.low[1] + 2 * margin;
             }
 
             this.paths = new ConnectTheDots(true, [[0, 0], [width, 0], [width, height], [0, height]]).paths;
