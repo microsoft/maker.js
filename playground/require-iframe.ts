d05b30819251abc54adcb8475a7cee414cfa7cc6
Dan Marshall <danmar@microsoft.com>
2016-08-05 20:23:59 -0700

    handle runtime errors during mock

--- playground/require-iframe.ts
+++ playground/require-iframe.ts
@@ -40,8 +40,11 @@ namespace MakerJsRequireIframe {
     }
 
     function runCodeIsolated(javaScript: string) {
+        var mockDocument = {
+            write: devNull
+        };
         var Fn: any = new Function('require', 'module', 'document', 'console', 'alert', 'playgroundRender', javaScript);
-        var result: any = new Fn(window.collectRequire, window.module, document, parent.console, devNull, devNull); //call function with the "new" keyword so the "this" keyword is an instance
+        var result: any = new Fn(window.collectRequire, window.module, mockDocument, parent.console, devNull, devNull); //call function with the "new" keyword so the "this" keyword is an instance
 
         return window.module.exports || result;
     }
@@ -118,6 +121,7 @@ namespace MakerJsRequireIframe {
     var counter = new Counter();
     var html = '';
     var error: Error = null;
+    var errorReported = false;
     var required: IRequireMap = {
         'makerjs': parent.makerjs,
         './../target/js/node.maker.js': parent.makerjs
@@ -145,6 +149,7 @@ namespace MakerJsRequireIframe {
 
         //send error results back to parent window
         parent.MakerJsPlayground.processResult('', errorDetails);
+        errorReported = true;
     };
 
     window.collectRequire = function (id: string) {
@@ -194,47 +199,43 @@ namespace MakerJsRequireIframe {
 
         function complete2() {
 
-            if (error) {
+            //reset any calls to document.write
+            html = '';
 
-                runCodeGlobal(javaScript);
+            //reinstate alert
+            window.alert = originalAlert;
 
-            } else {
-                //reset any calls to document.write
-                html = '';
+            //when all requirements are collected, run the code again, using its requirements
+            runCodeGlobal(javaScript);
 
-                //reinstate alert
-                window.alert = originalAlert;
+            if (errorReported) return;
 
-                //when all requirements are collected, run the code again, using its requirements
-                runCodeGlobal(javaScript);
+            //yield thread for the script tag to execute
+            setTimeout(function () {
 
-                //yield thread for the script tag to execute
-                setTimeout(function () {
-
-                    //restore properties from the "this" keyword
-                    var model: MakerJs.IModel = {};
-                    var props = ['layer', 'models', 'notes', 'origin', 'paths', 'type', 'units'];
-                    for (var i = 0; i < props.length; i++) {
-                        var prop = props[i];
-                        if (prop in window) {
-                            model[prop] = window[prop];
-                        }
+                //restore properties from the "this" keyword
+                var model: MakerJs.IModel = {};
+                var props = ['layer', 'models', 'notes', 'origin', 'paths', 'type', 'units'];
+                for (var i = 0; i < props.length; i++) {
+                    var prop = props[i];
+                    if (prop in window) {
+                        model[prop] = window[prop];
                     }
+                }
 
-                    var orderedDependencies: string[] = [];
-                    var scripts = head.getElementsByTagName('script');
-                    for (var i = 0; i < scripts.length; i++) {
-                        if (scripts[i].hasAttribute('id')) {
-                            orderedDependencies.push(scripts[i].id);
-                        }
+                var orderedDependencies: string[] = [];
+                var scripts = head.getElementsByTagName('script');
+                for (var i = 0; i < scripts.length; i++) {
+                    if (scripts[i].hasAttribute('id')) {
+                        orderedDependencies.push(scripts[i].id);
                     }
+                }
 
-                    //send results back to parent window
-                    parent.MakerJsPlayground.processResult(html, window.module.exports || model, orderedDependencies);
+                //send results back to parent window
+                parent.MakerJsPlayground.processResult(html, window.module.exports || model, orderedDependencies);
 
-                }, 0);
+            }, 0);
 
-            }
         };
 
         function complete1() {
