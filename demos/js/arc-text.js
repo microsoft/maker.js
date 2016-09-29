0cc169452577440b83cb6dbccbfe598c46e9b149
Dan Marshall <danmar@microsoft.com>
2016-09-28 19:48:40 -0700

    added onTop

--- demos/js/arc-text.js
+++ demos/js/arc-text.js
@@ -1,11 +1,12 @@
-﻿var makerjs = require('makerjs');
+var makerjs = require('makerjs');
 
-function ArcText(font, text, fontSize, arcRadius, startAngle, endAngle) {
+function ArcText(font, text, fontSize, arcRadius, startAngle, endAngle, onTop, showCircle) {
 
     var arc = new makerjs.paths.Arc([0, 0], arcRadius, startAngle, endAngle);
 
-    //uncomment to see the arc
-    //this.paths = { arc: arc };
+    if (showCircle) {
+    	this.paths = { circle: new makerjs.paths.Circle(arcRadius) };
+    }
 
     //generate the text using a font
     var textModel = new makerjs.models.Text(font, text, fontSize, false, true);
@@ -15,6 +16,10 @@ function ArcText(font, text, fontSize, arcRadius, startAngle, endAngle) {
         text: textModel,
     };
 
+  	//measure height of the text
+  	var measure = makerjs.measure.modelExtents(textModel);
+  	var height = measure.high[1] - measure.low[1];
+  
     //get the x distance of each character as a percentage
     var first = textModel.models[0].origin[0];
     var last = textModel.models[text.length - 1].origin[0];
@@ -31,13 +36,20 @@ function ArcText(font, text, fontSize, arcRadius, startAngle, endAngle) {
     var span = makerjs.angle.ofArcSpan(arc);
     for (var i = 0; i < text.length; i++) {
         var char = textModel.models[i];
+      
+      	if (!onTop) {
+          	//set a new origin at the top of the text
+	      	var o = makerjs.point.add(char.origin, [0, height]);
+      		makerjs.model.originate(char, o);
+        }
+      
         var angle = centers[i] * span;
-        var angleFromEnd = endAngle - angle;
+        var angleFromEnd = onTop ? endAngle - angle : startAngle + angle;
         var p = makerjs.point.fromAngleOnCircle(angleFromEnd, arc);
         char.origin = p;
 
         //rotate the char to 90 from tangent
-        makerjs.model.rotate(char, angleFromEnd - 90, p);
+        makerjs.model.rotate(char, onTop ? angleFromEnd - 90 : angleFromEnd + 90, p);
     }
 }
 
@@ -47,7 +59,9 @@ ArcText.metaParameters = [
     { title: "font size", type: "range", min: 10, max: 200, value: 72 },
     { title: "arc radius", type: "range", min: 1, max: 1000, value: 200 },
     { title: "start angle", type: "range", min: -90, max: 270, value: 45 },
-    { title: "end angle", type: "range", min: -90, max: 270, value: 135 }
+    { title: "end angle", type: "range", min: -90, max: 270, value: 135 },
+    { title: "top", type: "bool", value: true },
+    { title: "show circle", type: "bool", value: true }
 ];
 
 module.exports = ArcText;
