f5f187b9b0d0ac266e42ea49a8a893b82deffb7c
Dan Marshall <danmar@microsoft.com>
2017-01-17 00:11:47 -0800

    added travel function

--- src/core/maker.ts
+++ src/core/maker.ts
@@ -102,6 +102,45 @@ namespace MakerJs {
     }
 
     /**
+     * Travel along a route inside of a model to extract a specific node in its tree.
+     * 
+     * @param modelContext Model to travel within.
+     * @param routeKeyOrRoute String of a flattened route, or a string array of route segments.
+     * @returns Model or Path object within the modelContext tree.
+     */
+    export function travel(modelContext: IModel, routeKeyOrRoute: string | string[]) {
+        if (!modelContext || !routeKeyOrRoute) return null;
+
+        var route: string[];
+        if (Array.isArray(routeKeyOrRoute)) {
+            route = routeKeyOrRoute;
+        } else {
+            route = JSON.parse(routeKeyOrRoute);
+        }
+
+        var props = route.slice();
+        var ref: any = modelContext;
+        var origin = modelContext.origin || [0, 0];
+
+        while (props.length) {
+            var prop = props.shift();
+            ref = ref[prop];
+
+            if (!ref) return null;
+
+            if (ref.origin && props.length) {
+                origin = point.add(origin, ref.origin);
+            }
+        }
+
+        return {
+            path: <IPath | IModel>ref,
+            offset: origin
+        };
+
+    }
+
+    /**
      * @private
      */
     var clone = require('clone');
