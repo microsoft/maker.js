448d54ae9b48533247e07c9042d9ee4253be85b0
Dan Marshall <danmar@microsoft.com>
2017-02-11 12:18:56 -0800

    allow constructors for closed, coords

--- src/models/ConnectTheDots.ts
+++ src/models/ConnectTheDots.ts
@@ -1,5 +1,27 @@
 namespace MakerJs.models {
 
+    function getPoints(arg: IPoint[] | number[] | string) {
+        var coords: number[];
+
+        if (Array.isArray(arg)) {
+
+            if (isPoint(arg[0])) {
+                return arg as IPoint[];
+            }
+
+            coords = arg as number[];
+        } else {
+            coords = importer.parseNumericList(arg as string);
+        }
+
+        var points: IPoint[] = [];
+        for (var i = 0; i < coords.length; i += 2) {
+            points.push([coords[i], coords[i + 1]]);
+        }
+
+        return points;
+    }
+
     export class ConnectTheDots implements IModel {
 
         public paths: IPathMap = {};
@@ -17,6 +39,19 @@ namespace MakerJs.models {
         constructor(numericList: string);
 
         /**
+         * Create a model by connecting points designated in a string. The model will be 'closed' - i.e. the last point will connect to the first point.
+         * 
+         * Example:
+         * ```
+         * var c = new makerjs.models.ConnectTheDots(false, '-10 0 10 0 0 20'); // 3 coordinates to form a polyline
+         * ```
+         * 
+         * @param isClosed Flag to specify if last point should connect to the first point.
+         * @param numericList String containing a list of numbers which can be delimited by spaces, commas, or anything non-numeric (Note: [exponential notation](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toExponential) is allowed).
+         */
+        constructor(isClosed: boolean, numericList: string);
+
+        /**
          * Create a model by connecting points designated in a numeric array. The model will be 'closed' - i.e. the last point will connect to the first point.
          * 
          * Example:
@@ -27,6 +62,19 @@ namespace MakerJs.models {
          * @param coords Array of coordinates.
          */
         constructor(coords: number[]);
+
+        /**
+         * Create a model by connecting points designated in a numeric array. The model will be 'closed' - i.e. the last point will connect to the first point.
+         * 
+         * Example:
+         * ```
+         * var c = new makerjs.models.ConnectTheDots(false, [-10, 0, 10, 0, 0, 20]); // 3 coordinates to form a polyline
+         * ```
+         * 
+         * @param isClosed Flag to specify if last point should connect to the first point.
+         * @param coords Array of coordinates.
+         */
+        constructor(isClosed: boolean, coords: number[]);
 
         /**
          * Create a model by connecting points designated in an array of points. The model may be closed, or left open.
@@ -49,24 +97,12 @@ namespace MakerJs.models {
             switch (args.length) {
                 case 1:
                     isClosed = true;
-
-                    var coords: number[];
-
-                    if (Array.isArray(args[0])) {
-                        coords = args[0] as number[];
-                    } else {
-                        coords = importer.parseNumericList(args[0] as string);
-                    }
-                    
-                    points = [];
-                    for (var i = 0; i < coords.length; i += 2) {
-                        points.push([coords[i], coords[i + 1]]);
-                    }
+                    points = getPoints(args[0]);
                     break;
 
                 case 2:
                     isClosed = args[0] as boolean;
-                    points = args[1] as IPoint[];
+                    points = getPoints(args[1]);
                     break;
             }
 
@@ -89,7 +125,9 @@ namespace MakerJs.models {
         {
             title: "points", type: "select", value: [
                 [[0, 0], [40, 40], [60, 20], [100, 100], [60, 60], [40, 80]],
-                [[0, 0], [100, 0], [50, 87]]
+                [[0, 0], [100, 0], [50, 87]],
+                [-10, 0, 10, 0, 0, 20],
+                '-10 0 10 0 0 20',
             ]
         }
     ];
