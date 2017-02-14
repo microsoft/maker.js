94ea61ccdbe42b74c83d685548418d277ea7d6ae
Dan Marshall <danmar@microsoft.com>
2017-02-13 19:16:36 -0800

    use @types instead of tsd

--- typings/tsd.d.ts
+++
@@ -1,9 +0,0 @@
-
-/// <reference path="node/node.d.ts" />
-/// <reference path="OpenJsCad/csg.d.ts" />
-/// <reference path="pdfkit/pdfkit.d.ts" />
-/// <reference path="text-encoding/text-encoding.d.ts" />
-/// <reference path="codemirror/codemirror.d.ts" />
-/// <reference path="marked/marked.d.ts" />
-/// <reference path="bezier-js/bezier-js.d.ts" />
-/// <reference path="opentype/opentype.d.ts" />
