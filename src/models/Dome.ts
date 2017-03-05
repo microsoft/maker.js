17ab41ee68bccba01d667b3bf2c3e5a6a74d045f
Dan Marshall <danmar@microsoft.com>
2017-03-05 13:06:49 -0800

    add bottomless to dome

--- src/models/Dome.ts
+++ src/models/Dome.ts
@@ -4,7 +4,7 @@
 
         public paths: IPathMap = {};
 
-        constructor(width: number, height: number, radius?: number) {
+        constructor(width: number, height: number, radius?: number, bottomless?: boolean) {
 
             var w2 = width / 2;
 
@@ -15,8 +15,10 @@
             var wt = Math.max(w2 - radius, 0);
             var hr = Math.max(height - radius, 0);
 
-            this.paths["Bottom"] = new paths.Line([-w2, 0], [w2, 0]);
-
+            if (!bottomless) {
+                this.paths["Bottom"] = new paths.Line([-w2, 0], [w2, 0]);
+            }
+        
             if (hr) {
                 this.paths["Left"] = new paths.Line([-w2, 0], [-w2, hr]);
                 this.paths["Right"] = new paths.Line([w2, 0], [w2, hr]);
@@ -37,6 +39,7 @@
     (<IKit>Dome).metaParameters = [
         { title: "width", type: "range", min: 1, max: 100, value: 50 },
         { title: "height", type: "range", min: 1, max: 100, value: 100 },
-        { title: "radius", type: "range", min: 0, max: 50, value: 25 }
+        { title: "radius", type: "range", min: 0, max: 50, value: 25 },
+        { title: "bottomless", type: "bool", value: false }
     ];
 }
