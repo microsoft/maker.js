d5cf4f3decf7277910bcb20acd27e7f01302cdd8
Dan Marshall <danmar@microsoft.com>
2017-03-18 16:44:18 -0700

    document layouts

--- src/core/layout.ts
+++ src/core/layout.ts
@@ -1,6 +1,9 @@
 namespace MakerJs.layout {
 
-    function cloneTo(dimension: number, itemToClone: IModel | IPath, count: number, margin = 0): IModel {
+    /**
+     * @private
+     */
+    function cloneTo(dimension: number, itemToClone: IModel | IPath, count: number, margin: number): IModel {
         var result: IModel = {};
         var add: IPathMap | IModelMap;
         var measureFn: (x: IModel | IPath) => IMeasure;
@@ -28,19 +31,68 @@ namespace MakerJs.layout {
         return result;
     }
 
-    export function cloneToColumn(itemToClone: IModel | IPath, count: number, margin?: number): IModel {
+    /**
+     * Layout clones in a column format.
+     * 
+     * @param itemToClone: Either a model or a path object.
+     * @param count Number of clones in the column.
+     * @param margin Optional distance between each clone.
+     * @returns A new model with clones in a column.
+     */
+    export function cloneToColumn(itemToClone: IModel | IPath, count: number, margin = 0): IModel {
         return cloneTo(1, itemToClone, count, margin);
     }
 
-    export function cloneToRow(itemToClone: IModel | IPath, count: number, margin?: number): IModel {
+    /**
+     * Layout clones in a row format.
+     * 
+     * @param itemToClone: Either a model or a path object.
+     * @param count Number of clones in the row.
+     * @param margin Optional distance between each clone.
+     * @returns A new model with clones in a row.
+     */
+    export function cloneToRow(itemToClone: IModel | IPath, count: number, margin = 0): IModel {
         return cloneTo(0, itemToClone, count, margin);
     }
 
-    export function cloneToGrid(itemToClone: IModel | IPath, xCount: number, yCount: number, margin?: number): IModel {
-        return cloneToColumn(cloneToRow(itemToClone, xCount, margin), yCount, margin);
+    /**
+     * Layout clones in a grid format.
+     * 
+     * @param itemToClone: Either a model or a path object.
+     * @param xCount Number of columns in the grid.
+     * @param yCount Number of rows in the grid.
+     * @param margin Optional numeric distance between each clone. Can also be a 2 dimensional array of numbers, to specify distances in x and y dimensions.
+     * @returns A new model with clones in a grid layout.
+     */
+    export function cloneToGrid(itemToClone: IModel | IPath, xCount: number, yCount: number, margin?: number | IPoint): IModel {
+        var margins = getMargins(margin);
+        return cloneToColumn(cloneToRow(itemToClone, xCount, margins[0]), yCount, margins[1]);
     }
 
-    function cloneToAlternate(itemToClone: IModel | IPath, xCount: number, yCount: number, margin: number, spacingFn: (modelToMeasure: IModel) => IPoint): IModel {
+    /**
+     * @private
+     */
+    function getMargins(margin?: number | IPoint) : IPoint {
+        if (Array.isArray(margin)) {
+            return margin;
+        } else {
+            return [margin as number, margin as number];
+        }
+    }
+
+    /**
+     * @private
+     */
+    interface IGridSpacing {
+        x: number;
+        y: number;
+        xMargin: number;
+    }
+
+    /**
+     * @private
+     */
+    function cloneToAlternate(itemToClone: IModel | IPath, xCount: number, yCount: number, spacingFn: (modelToMeasure: IModel) => IGridSpacing): IModel {
         var modelToMeasure: IModel;
         if (isModel(itemToClone)) {
             modelToMeasure = itemToClone;
@@ -53,32 +105,52 @@ namespace MakerJs.layout {
 
         for (var i = 0; i < yCount; i++) {
             var i2 = i % 2;
-            result.models[i] = model.move(cloneToRow(itemToClone, xCount + i2, margin), [i2 * spacing[0], i * spacing[1]]);
+            result.models[i] = model.move(cloneToRow(itemToClone, xCount + i2, spacing.xMargin), [i2 * spacing.x, i * spacing.y]);
         }
 
         return result;
     }
 
-    export function cloneToBrick(itemToClone: IModel | IPath, xCount: number, yCount: number, margin = 0): IModel {
+    /**
+     * Layout clones in a brick format. Alternating rows will have an additional item in each row.
+     * 
+     * @param itemToClone: Either a model or a path object.
+     * @param xCount Number of columns in the brick grid.
+     * @param yCount Number of rows in the brick grid.
+     * @param margin Optional numeric distance between each clone. Can also be a 2 dimensional array of numbers, to specify distances in x and y dimensions.
+     * @returns A new model with clones in a brick layout.
+     */
+    export function cloneToBrick(itemToClone: IModel | IPath, xCount: number, yCount: number, margin?: number | IPoint): IModel {
 
-        function spacing(modelToMeasure: IModel) {
+        var margins = getMargins(margin);
+
+        function spacing(modelToMeasure: IModel): IGridSpacing {
             var m = measure.modelExtents(modelToMeasure);
-            return [(m.width + margin) / -2, m.height + margin];
+            return { x: (m.width + margins[0]) / -2, y: m.height + margins[1], xMargin: margins[0] };
         }
 
-        return cloneToAlternate(itemToClone, xCount, yCount, margin, spacing);
+        return cloneToAlternate(itemToClone, xCount, yCount, spacing);
     }
 
+    /**
+     * Layout clones in a honeycomb format. Alternating rows will have an additional item in each row.
+     * 
+     * @param itemToClone: Either a model or a path object.
+     * @param xCount Number of columns in the honeycomb grid.
+     * @param yCount Number of rows in the honeycomb grid.
+     * @param margin Optional distance between each clone.
+     * @returns A new model with clones in a honeycomb layout.
+     */
     export function cloneToHoneycomb(itemToClone: IModel | IPath, xCount: number, yCount: number, margin = 0): IModel {
         
-        function spacing(modelToMeasure: IModel) {
+        function spacing(modelToMeasure: IModel): IGridSpacing {
             var hex = measure.boundingHexagon(modelToMeasure);
             var width = 2 * solvers.equilateralAltitude(hex.radius);
             var s = width + margin;
-            return [s / -2, solvers.equilateralAltitude(s)];
+            return { x: s / -2, y: solvers.equilateralAltitude(s), xMargin: margin };
         }
     
-        return cloneToAlternate(itemToClone, xCount, yCount, margin, spacing);
+        return cloneToAlternate(itemToClone, xCount, yCount, spacing);
     }
 
 }
