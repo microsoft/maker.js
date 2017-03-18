67850c82a79d3b4cc79e9005fb53c02b83baa14f
Dan Marshall <danmar@microsoft.com>
2017-03-17 23:37:14 -0700

    add layout module

---
+++ src/core/layout.ts
@@ -0,0 +1,39 @@
+namespace MakerJs.layout {
+
+    function cloneTo(dimension: number, itemToClone: IModel | IPath, count: number, margin = 0): IModel {
+        var result: IModel = {};
+        var add: IPathMap | IModelMap;
+        var measureFn: (x: IModel | IPath) => IMeasure;
+        var moveFn: (x: IModel | IPath, origin: IPoint) => IModel | IPath;
+
+        if (isModel(itemToClone)) {
+            measureFn = measure.modelExtents;
+            add = result.models = {};
+            moveFn = model.move;
+        } else {
+            measureFn = measure.pathExtents;
+            add = result.paths = {};
+            moveFn = path.move;
+        }
+
+        var m = measureFn(itemToClone);
+        var size = m.high[dimension] - m.low[dimension];
+        
+        for (var i = 0; i < count; i++) {
+            var origin: IPoint = [0, 0];
+            origin[dimension] = i * (size + margin);
+            add[i] = moveFn(cloneObject(itemToClone), origin);
+        }
+
+        return result;
+    }
+
+    export function cloneToColumn(itemToClone: IModel | IPath, count: number, margin?: number): IModel {        
+        return cloneTo(1, itemToClone, count, margin);
+    }
+
+    export function cloneToRow(itemToClone: IModel | IPath, count: number, margin?: number): IModel {        
+        return cloneTo(0, itemToClone, count, margin);
+    }
+
+}
