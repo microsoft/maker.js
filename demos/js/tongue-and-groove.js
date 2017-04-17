2b99def48c21419c1c6acca5e782593be55acb4e
Dan Marshall <danmar@microsoft.com>
2017-04-16 23:08:21 -0700

    added tongue and groove

---
+++ demos/js/tongue-and-groove.js
@@ -0,0 +1,26 @@
+var m = require("makerjs");
+
+function TongueAndGroove(count, tongueWidth, grooveWidth, grooveDepth, dogbone) {
+  
+  //Tongue and grooves for a box joint
+  var groove = new m.models.Dogbone(grooveWidth, grooveDepth, dogbone, 0, true);
+
+  groove.paths['leftTongue'] = new m.paths.Line([-tongueWidth / 2, 0], [0, 0]);
+  groove.paths['rightTongue'] = new m.paths.Line([grooveWidth, 0], [grooveWidth + tongueWidth / 2, 0]);
+
+  this.models = {
+    tng: m.layout.cloneToRow(groove, count)
+  };
+  
+}
+
+TongueAndGroove.metaParameters = [
+    { title: 'count', type: 'range', min: 1, max: 100, value: 3 },
+    { title: 'tongueWidth', type: 'range', min: 1, max: 100, value: 60 },
+    { title: 'grooveWidth', type: 'range', min: 10, max: 100, value: 50 },
+    { title: 'grooveDepth', type: 'range', min: 1, max: 100, value: 30 },
+    { title: 'dogbone', type: 'range', min: 0, max: 20, value: 5 },
+    { title: 'expand', type: 'bool', value: true },
+];
+
+module.exports = TongueAndGroove;
