6496e6ef591bb9c1d29b22d8766aaf1143f89d0e
Dan Marshall <danmar@microsoft.com>
2017-07-16 12:05:20 -0700

    don't depend on other module metaParameters

--- demos/js/window-arch-grid.js
+++ demos/js/window-arch-grid.js
@@ -1,7 +1,7 @@
 var makerjs = require('makerjs');
 var WindowArch = require('./window-arch');
 
-function WindowArchGrid(width, columns, extra, split, spacing, fillet, height, rows, outerFrame, outerFrameWidth) {
+function WindowArchGrid(width, height, rows, columns, extra, split, spacing, fillet, outerFrame, outerFrameWidth) {
     var column = (width - (columns - 1) * spacing) / columns;
     var row = (height - (rows) * spacing + spacing / 2) / rows;
     var cell = new makerjs.models.RoundRectangle(column, row, fillet);
@@ -22,11 +22,17 @@ function WindowArchGrid(width, columns, extra, split, spacing, fillet, height, r
     }
 }
 
-WindowArchGrid.metaParameters = WindowArch.metaParameters.slice().concat([
+WindowArchGrid.metaParameters = [
+    { title: "width", type: "range", min: 12, max: 500, value: 200 },
     { title: "grid height", type: "range", min: 12, max: 500, value: 200 },
     { title: "rows", type: "range", min: 1, max: 10, value: 4 },
+    { title: "columns", type: "range", min: 1, max: 10, value: 6 },
+    { title: "extra wedge", type: "bool", value: false },
+    { title: "split central dome on even columns", type: "bool", value: false },
+    { title: "spacing", type: "range", min: 0.5, max: 10, value: 2, step: 0.5 },
+    { title: "fillet", type: "range", min: 0, max: 10, value: 1, step: 0.5 },
     { title: "outer frame", type: "bool", value: true },
     { title: "outer frame width", type: "range", min: 1, max: 20, value: 6 }
-]);
+];
 
 module.exports = WindowArchGrid;
