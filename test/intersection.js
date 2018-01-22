bce257749794e3881dc8d51fad39bce129d56f43
Dan Marshall <danmar@microsoft.com>
2018-01-21 22:37:44 -0800

    added tests for angle & measure

--- test/intersection.js
+++ test/intersection.js
@@ -19,12 +19,28 @@ describe('Path Intersection', function () {
                 "endAngle": 106.36363636363635
             };
 
-        const int = makerjs.path.intersection(a, c);        
+        const int = makerjs.path.intersection(a, c);
         assert.ok(int);
         assert.ok(int.path1Angles);
         assert.ok(int.path1Angles[0]);
         assert.ok(int.path2Angles);
         assert.ok(int.path2Angles[0]);
     });
+
+    it('should intersect an arc and a line at one point', function () {
+
+        var line = new makerjs.paths.Line([0, 3], [-75, 3]);
+        var arc = new makerjs.paths.Arc([0, 50], 53, 180, 270);
+
+        var int = makerjs.path.intersection(line, arc);
+
+        assert.ok(int);
+        assert.ok(int.path2Angles);
+        assert.equal(int.path2Angles, 242.4729147);
+        assert.ok(int.intersectionPoints);
+        assert.equal(int.intersectionPoints.length, 1);
+        assert.equal(int.intersectionPoints[0][0], -24.494897437784694);
+        assert.equal(int.intersectionPoints[0][1], 3.0000000051871396);
+    })
 
 });
