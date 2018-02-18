b0e701fe0a1c96090cd7c6f67ab4ffe2b01c9635
Dan Marshall <danmar@microsoft.com>
2018-02-17 21:50:05 -0800

    new build 0.9.87

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Maker.js 0.9.86
+// Type definitions for Maker.js 0.9.87
 // Project: https://github.com/Microsoft/maker.js
 // Definitions by: Dan Marshall <https://github.com/danmarshall>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
