1239b8e3b31d993bb31b52617b92a6b4993750dc
Dan Marshall <danmar@microsoft.com>
2018-08-20 00:47:56 -0700

    added Dogbone fillet section

--- docs/demos/js/tongue-and-groove.js
+++ docs/demos/js/tongue-and-groove.js
@@ -1,9 +1,9 @@
 var m = require("makerjs");
 
-function TongueAndGroove(count, tongueWidth, grooveWidth, grooveDepth, dogbone) {
+function TongueAndGroove(count, tongueWidth, grooveWidth, grooveDepth, radius, style) {
 
   //Tongue and grooves for a box joint
-  var groove = new m.models.Dogbone(grooveWidth, grooveDepth, dogbone, 0, true);
+  var groove = new m.models.Dogbone(grooveWidth, grooveDepth, radius, style, true);
 
   groove.paths.leftTongue = new m.paths.Line([-tongueWidth / 2, 0], [0, 0]);
   groove.paths.rightTongue = new m.paths.Line([grooveWidth, 0], [grooveWidth + tongueWidth / 2, 0]);
@@ -20,7 +20,8 @@ TongueAndGroove.metaParameters = [
   { title: 'tongueWidth', type: 'range', min: 1, max: 100, value: 60 },
   { title: 'grooveWidth', type: 'range', min: 10, max: 100, value: 50 },
   { title: 'grooveDepth', type: 'range', min: 1, max: 100, value: 30 },
-  { title: 'dogbone', type: 'range', min: 0, max: 20, value: 5 }
+  { title: 'radius', type: 'range', min: 0, max: 20, value: 5 },
+  { title: 'corner style', type: 'select', value: [0, -1, 1] }
 ];
 
 module.exports = TongueAndGroove;
