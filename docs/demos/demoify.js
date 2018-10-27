2f229d660a311840cbda3db590a32a9337bd7aac
Dan Marshall <danmar@microsoft.com>
2018-10-26 18:17:55 -0700

    fix link to demos folder

--- docs/demos/demoify.js
+++ docs/demos/demoify.js
@@ -126,7 +126,7 @@ function demoIndexPage() {
         var st = sectionTag();
         stream.write(jekyll('default', 'Demos'));
         writeHeading(1, 'Demos');
-        var yourDemoHtml = marked('### How to add your own demo to this gallery:\n 1. Fork the Maker.js repo on GitHub.\n 2. Add your code to [the demos folder](https://github.com/Microsoft/maker.js/tree/master/demos/js).\n 3. Submit a pull request!');
+        var yourDemoHtml = marked('### How to add your own demo to this gallery:\n 1. Fork the Maker.js repo on GitHub.\n 2. Add your code to [the demos folder](https://github.com/Microsoft/maker.js/tree/master/docs/demos/js).\n 3. Submit a pull request!');
         stream.write(section(yourDemoHtml));
         stream.write(st.getOpeningTag(false));
         writeHeading(2, 'Models published on ' + anchor('NPM', 'https://www.npmjs.com/search?q=makerjs', 'search NPM for keyword "makerjs"'));
