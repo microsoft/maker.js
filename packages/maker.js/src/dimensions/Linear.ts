55ec1e73c1ff6a2080ba4200601ecb2cdceb3199
Dan Marshall <danmar@microsoft.com>
2018-11-04 20:09:27 -0800

    optional extensions

--- packages/maker.js/src/dimensions/Linear.ts
+++ packages/maker.js/src/dimensions/Linear.ts
@@ -12,21 +12,22 @@ namespace MakerJs.dimensions {
 
         constructor(
             size: Size,
-            p1: IPoint, p2: IPoint);
+            p1: IPoint, p2: IPoint, extensions?: boolean);
 
         constructor(
             arrowSize: number, arrowSpanAngle: number, textSize: number, accuracy: number, textOffset: number, entensionLength: number, extensionAnchor: number,
-            p1: IPoint, p2: IPoint);
+            p1: IPoint, p2: IPoint, extensions?: boolean);
 
         constructor(...args: any[]) {
             let temp: _Linear;
             switch (args.length) {
                 case 3:
+                case 4:
                     temp = {} as _Linear;
                     _Linear.apply(temp, args);
                     break;
                 default:
-                    temp = new _Linear(Size.fromArgs(args), args.shift(), args.shift());
+                    temp = new _Linear(Size.fromArgs(args), args.shift(), args.shift(), args.shift());
                     break;
             }
             this.type = Linear.typeName;
@@ -46,15 +47,17 @@ namespace MakerJs.dimensions {
         public paths: IPathMap;
         public origin: IPoint;
 
-        constructor(size: Size, p1: IPoint, p2: IPoint) {
+        constructor(size: Size, p1: IPoint, p2: IPoint, extensions?: boolean) {
             const a = angle.ofPointInDegrees(p1, p2);
             const d = measure.pointDistance(p1, p2);
-            const y = size.entensionLength * size.extensionAnchor;
+            const y = extensions ? size.entensionLength * size.extensionAnchor : 0;
             this.paths = {
-                extension1: new paths.Line([0, 0], [0, size.entensionLength]),
-                extension2: new paths.Line([d, 0], [d, size.entensionLength]),
                 dimensionLine: new paths.Line([0, y], [d, y])
             };
+            if (extensions) {
+                this.paths.extension1 = new paths.Line([0, 0], [0, size.entensionLength]);
+                this.paths.extension2 = new paths.Line([d, 0], [d, size.entensionLength]);
+            }
             this.models = {
                 arrow1: model.move(new ArrowHead(size, 180), [0, y]),
                 arrow2: model.move(new ArrowHead(size, 0), [d, y])
@@ -81,6 +84,7 @@ namespace MakerJs.dimensions {
             title: "point 2", type: "select", value: [
                 [100, 0], [100, 50], [50, 87], [-100, -10]
             ]
-        }
+        },
+        { title: "extensions", type: "bool", value: true }
     ]);
 }
