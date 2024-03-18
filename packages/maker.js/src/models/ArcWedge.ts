bcc20ad3cc0d31e8be534a8d79067e6cfa3bca0a
Andy Woolley <43644601+milonic@users.noreply.github.com>
2024-03-18 14:25:32 +0000

    Update ArcWedge.ts
    Changing ArcWedge tojust add a line at end of arcs. Works in same way as OvalArc but without the radius at each end.

--- packages/maker.js/src/models/ArcWedge.ts
+++ packages/maker.js/src/models/ArcWedge.ts
@@ -5,14 +5,8 @@ namespace MakerJs.models {
         public paths: IPathMap = {};
         public models: IModelMap;
 
-        constructor(startAngle: number, endAngle: number, sweepRadius: number, slotRadius: number, capRadius, largeArc = false, selfIntersect = false, isolateCaps = false) {
+        constructor(startAngle: number, endAngle: number, sweepRadius: number, slotRadius: number, selfIntersect = false, isolateCaps = false) {
 
-            // Change capRadius to an array, if it is not already and create elements for start and end caps
-            if (!Array.isArray(capRadius))capRadius = [capRadius, capRadius];
-
- 	        // Make sure the capRadius Array contains numerics
-            capRadius=capRadius.map(Number);
-            
             var capRoot: IModel;
 
             if (isolateCaps) {
@@ -29,7 +23,7 @@ namespace MakerJs.models {
 
             if (endAngle < startAngle) endAngle += 360;
 
-            var addCap = (id: string, tiltAngle: number, _capRadius: number): IPathArc => {
+            var addCap = (id: string, tiltAngle: number): IPathLine => {
                 var capModel: IModel;
 
                 if (isolateCaps) {
@@ -38,26 +32,13 @@ namespace MakerJs.models {
                 } else {
                     capModel = this;
                 }
-                
-				var sweepSlotA = sweepRadius + slotRadius
-				var sweepSlotB = sweepRadius - slotRadius
 
-				if( (id[0] == "S" && _capRadius>=0) || (id[0] == "E" && _capRadius < 0) ){
-					sweepSlotA = sweepRadius - slotRadius
-					sweepSlotB = sweepRadius + slotRadius
-				}
-                
-                // Find the end points for creating the arc
-				var pointA = MakerJs.point.fromPolar(MakerJs.angle.toRadians(tiltAngle), sweepSlotA)
-				var pointB = MakerJs.point.fromPolar(MakerJs.angle.toRadians(tiltAngle), sweepSlotB)
+                var pointA = MakerJs.point.fromPolar(MakerJs.angle.toRadians(tiltAngle), sweepRadius + slotRadius);
+                var pointB = MakerJs.point.fromPolar(MakerJs.angle.toRadians(tiltAngle), sweepRadius - slotRadius);
 
-				// Increase or decrease _capRadius by slotRadius, eliminates the dead space between a capRadius of zero and slotRadius
-				if(_capRadius<0)_capRadius-=slotRadius; else _capRadius+=slotRadius
-				
-				// Disable largeArc for negative _capRadius values, it does odd things!
-				if ( largeArc && _capRadius<0 ) largeArc = false
-				
-				return capModel.paths[id] = new MakerJs.paths.Arc(pointA, pointB, Math.abs(_capRadius), largeArc, false);
+                return capModel.paths[id] = new MakerJs.paths.Line(
+                     pointA, 
+                     pointB);                     
             };
 
             var addSweep = (id: string, offsetRadius: number): IPathArc => {
@@ -76,29 +57,8 @@ namespace MakerJs.models {
             }
 
             var caps = [];
-            caps.push(addCap("StartCap", startAngle, capRadius[0]));
-            caps.push(addCap("EndCap", endAngle, capRadius[1]));
-
-            //the distance between the cap origins
-            var d = measure.pointDistance(caps[0].origin, caps[1].origin);
-
-            if ((d / 2) < slotRadius) {
-                //the caps intersect
-
-                var int = path.intersection(caps[0], caps[1]);
-                if (int) {
-
-                    if (!hasInner || !selfIntersect) {
-                        caps[0].startAngle = int.path1Angles[0];
-                        caps[1].endAngle = int.path2Angles[0];
-                    }
-
-                    if (!selfIntersect && hasInner && int.intersectionPoints.length == 2) {
-                        addCap("StartCap2", startAngle, capRadius[0]).endAngle = int.path1Angles[1];
-                        addCap("EndCap2", endAngle, capRadius[1]).startAngle = int.path2Angles[1] + 360;
-                    }
-                }
-            }
+            caps.push(addCap("StartCap", startAngle));
+            caps.push(addCap("EndCap", endAngle));
         }
     }
 
@@ -107,8 +67,6 @@ namespace MakerJs.models {
         { title: "end angle", type: "range", min: -360, max: 360, step: 1, value: 0 },
         { title: "sweep", type: "range", min: 0, max: 100, step: 1, value: 50 },
         { title: "radius", type: "range", min: 0, max: 100, step: 1, value: 15 },
-        { title: "cap radius", type: "range", min: -180, max: 180, step: 1, value: 180 },
-        { title: "largeArc", type: "bool", value: false },
         { title: "self intersect", type: "bool", value: false }
     ];
 }
